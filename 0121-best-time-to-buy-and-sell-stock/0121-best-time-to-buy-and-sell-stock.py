19e7b79e700034dc4649873a87db56dbaf88a977
thrilokb766 <thrilokb047@gmail.com>
2025-12-13 02:44:20 +0530

    Time: 111 ms (38.62%), Space: 19.4 MB (23.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,8 +2,8 @@ class Solution(object):
     def maxProfit(self, prices):
         min1=prices[0]
         max1=0
-        for i in range(len(prices)):
+        for i in range(1,len(prices)):
             if prices[i]<min1:
-                min1=prices[i]
-            max1=max(max1,prices[i]-min1)
+                min1=prices[i]         
+            max1=abs(max(max1,prices[i]-min1))
         return max1
